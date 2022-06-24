f28f80a8b113d5463fe85ec0e858c3a4488b2545
rpaezbas <rpaezbas@rpaezbas.com>
2022-06-24 02:44:29 +0200

    format fix

--- tests/signature.js
+++ tests/signature.js
@@ -2,15 +2,19 @@ const { test, solo } = require('brittle') // eslint-disable-line
 const sodium = require('sodium-native')
 const SerialSigner = require('../')
 
-test('signature is valid', async ({ assert, teardown }) => {
-    const signer = new SerialSigner('/dev/ttyUSB0')
-    await signer.ready()
-    const pk = await signer.publicKey()
-    const message = Buffer.from('hello world')
-    const signature = await signer.sign(message)
-    assert.ok(sodium.crypto_sign_verify_detached(signature, message, pk))
+test('signature and scalarmult', async ({ assert, pass, teardown }) => {
+  const signer = new SerialSigner('/dev/ttyUSB0')
+  await signer.ready()
+  const pk = await signer.publicKey()
 
-    teardown(async () => {
-        await signer.close()
-    })
+  const message = Buffer.allocUnsafe(256)
+  const signature = await signer.sign(message)
+  assert.ok(sodium.crypto_sign_verify_detached(signature, message, pk))
+
+  await signer.dh(pk)
+  pass()
+
+  teardown(async () => {
+    await signer.close()
+  })
 })
