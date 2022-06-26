4ae6e38a0885121f03a57d26501491c6b8aafdb4
rpaezbas <rpaezbas@rpaezbas.com>
2022-06-26 18:30:42 +0200

    process messages sequencially

--- index.js
+++ index.js
@@ -1,53 +1,69 @@
 const { once } = require('events')
 const { SerialPort } = require('serialport')
 
+const SIGNATURE_LENGTH = 64
+const PK_LENGTH = 32
+const DH_LENGTH = 32
+
+let buffer = Buffer.alloc(0)
+
 module.exports = class SerialSigner {
   constructor (port) {
     this.serialport = new SerialPort({ path: '/dev/ttyUSB0', baudRate: 115200 })
     this.opened = false
+    this.processing = false
+    this.queue = [] // FIFO messages queue
     this.serialport.on('open', () => {
       this.opened = true
     })
   }
 
   async ready () {
+    if (!this.processing) {
+      this.serialport.on('data', this._proccess.bind(this))
+      this.processing = true
+    }
     if (!this.opened) {
       return once(this.serialport, 'open')
     }
   }
 
-  async sign (message) {
-    this.serialport.write(message)
-    let signature = Buffer.alloc(0)
-    return new Promise(resolve => {
-      this.serialport.on('data', (data) => {
-        signature = Buffer.concat([signature, data])
-        if (signature.length === 64) resolve(signature)
-      })
+  // process the messages sequencially
+  _proccess (data) {
+    buffer = Buffer.concat([buffer, data])
+    const first = this.queue[0]
+    const next = this.queue[1]
+    if (buffer.length === first.responseLength) {
+      first.resolve(buffer)
+      buffer = Buffer.alloc(0)
+      if (next) this.serialport.write(next.message)
+      this.queue.shift()
+    } else if (buffer.length > first.responseLength) {
+      first.reject('Error in reading. Buffer size too large.')
+      buffer = Buffer.alloc(0)
+      if (next) this.serialport.write(next.message)
+      this.queue.shift()
+    }
+  }
+
+  _request (message, responseLength) {
+    const promise = new Promise((resolve, reject) => {
+      this.queue.push({ message, responseLength, resolve, reject })
     })
+    if (this.queue.length === 1) this.serialport.write(message)
+    return promise
+  }
+
+  async sign (message) {
+    return this._request(message, SIGNATURE_LENGTH)
   }
 
   async publicKey () {
-    this.serialport.write('__public_key__')
-    let pk = Buffer.alloc(0)
-    return new Promise(resolve => {
-      this.serialport.on('data', (data) => {
-        pk = Buffer.concat([pk, data])
-        if (pk.length === 32) resolve(pk)
-      })
-    })
+    return this._request('__public_key__', PK_LENGTH)
   }
 
   async dh (publicKey) {
-    const header = Buffer.from('__dh__')
-    this.serialport.write(Buffer.concat([header, publicKey]))
-    let signature = Buffer.alloc(0)
-    return new Promise(resolve => {
-      this.serialport.on('data', (data) => {
-        signature = Buffer.concat([signature, data])
-        if (signature.length === 32) resolve(signature)
-      })
-    })
+    return this._request('__dh__', DH_LENGTH)
   }
 
   async close () {
--- tests/signature.js
+++ tests/signature.js
@@ -2,19 +2,42 @@ const { test, solo } = require('brittle') // eslint-disable-line
 const sodium = require('sodium-native')
 const SerialSigner = require('../')
 
-test('signature and scalarmult', async ({ assert, pass, teardown }) => {
-  const signer = new SerialSigner('/dev/ttyUSB0')
+const signer = new SerialSigner('/dev/ttyUSB0')
+
+test('consecutive synchrounous signatures', async ({ plan, assert, pass, teardown }) => {
   await signer.ready()
   const pk = await signer.publicKey()
 
+  const numOfMessages = 20
+  const messageByteLength = 256
+  const messages = []
+
+  plan(numOfMessages)
+
+  for (let i = 0; i < numOfMessages; i++) {
+    messages.push(Buffer.allocUnsafe(messageByteLength))
+  }
+
+  const signatures = await Promise.all(messages.map(m => signer.sign(m)))
+
+  signatures.forEach((s, i) => {
+    assert.ok(sodium.crypto_sign_verify_detached(s, messages[i], pk))
+  })
+})
+
+test('signature and scalarmult', async ({ plan, assert, pass, teardown }) => {
+  await signer.ready()
+  plan(2)
+
+  const pk = await signer.publicKey()
   const message = Buffer.allocUnsafe(256)
   const signature = await signer.sign(message)
-  assert.ok(sodium.crypto_sign_verify_detached(signature, message, pk))
 
   await signer.dh(pk)
+
+  assert.ok(sodium.crypto_sign_verify_detached(signature, message, pk))
+
   pass()
 
-  teardown(async () => {
-    await signer.close()
-  })
+  signer.close()
 })
